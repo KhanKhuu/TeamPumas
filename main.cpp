6365198a368d586ebe0f752601c2e0e1e4b2899b
Matthew Larkins <33473815+KhanKhuu@users.noreply.github.com>
2018-10-14 15:56:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include "myvector.h"
+#include "myVector.h"
 using namespace std;
 
 int main() {
@@ -22,7 +22,6 @@ int main() {
     for (int i = 0; i < myVec2.size(); i++) {
         cout <<myVec2.at(i) << " ";
     }
-    
     cout << endl;
     
     myVec2.erase(5);
@@ -33,7 +32,6 @@ int main() {
     cout << endl;
     
     myVector<int> myVec3;
-    cout << "Testing insert function: " << endl;
     myVec3.insert(0, 4);
     cout << myVec3.at(0) << endl;
     myVec2.insert(21, 72);
@@ -73,13 +71,60 @@ int main() {
         cout <<myVec2.at(i) << " ";
     }
     
+    cout << "\n\n\n\n\n";
+    
+    myVector<int> myVec5 = myVec2;
+    cout << myVec5.size() << " " << myVec5.capacity() << endl;
+    
+    
+    //**************************    SwapTest    ****************************//
+    cout << "MyVec3 before:\n";
+    for (int i = 0; i < myVec3.size(); i++) {
+        cout <<myVec3.at(i) << " ";
+    }
     cout << endl;
     
+    cout << "myVec2 before:\n";
+    for (int i = 0; i < myVec2.size(); i++) {
+        cout <<myVec2.at(i) << " ";
+    }
+    cout << endl;
     
+    myVec3.swap(myVec2);
+    
+    cout << "MyVec3 after:\n";
+    for (int i = 0; i < myVec3.size(); i++) {
+        cout <<myVec3.at(i) << " ";
+    }
+    cout << endl;
+    
+    cout << "myVec2 after:\n";
+    for (int i = 0; i < myVec2.size(); i++) {
+        cout <<myVec2.at(i) << " ";
+    }
+    cout << endl;
+    
+    myVec3.assign(50, 10);
+    for (int i = 0; i < myVec3.size(); i++) {
+        cout << myVec3.at(i) << " ";
+    }
+    
+    cout << "\n\n\n\n\n\n\n\n";
+    myVec3.assign({1, 3, 5, 7, 9, 11, 13, 15, 17});
+    for (int i = 0; i < myVec3.size(); i++) {
+        cout << myVec3.at(i) << " ";
+    }
+    
+    /*
+    myVector<int> myVec7(10, 12);
+    myVec7.resize(33);
+    myVector<int> myVec8(20, 12);
+    myVec8.resize(18);
+    myVector<int> myVec9(5, 12);
+    myVec9.resize(5);
+    */
 }
 /*
  reserve
  operator[]
- assign
- swap
  */
